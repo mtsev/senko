558038e8fd012683a2b75edc5806342c275f3055
mtsev <mtsev@users.noreply.github.com>
2021-01-23 14:45:12 +1100

    timestamp logging

--- cogs/utils/logs.py
+++ cogs/utils/logs.py
@@ -1,8 +1,11 @@
 from discord.ext.commands import Context
+import logging
+
+log = logging.getLogger(__name__)
 
 def log_command(ctx: Context) -> None:
     if ctx.guild:
         channel = f'{ctx.guild}/{ctx.channel}'
     else:
         channel = 'DM'
-    print(f'({channel}) <{ctx.author}> {ctx.message.content}')
+    log.warning(f'({channel}) <{ctx.author}> {ctx.message.content}')
--- senko.py
+++ senko.py
@@ -1,8 +1,27 @@
 #!/usr/bin/env python3
 import os
+import logging
 
 import yaml
 from discord.ext.commands import Bot, Context, CommandError, CommandOnCooldown
+
+# create logger
+log = logging.getLogger(__package__)
+log.setLevel(logging.DEBUG)
+# create file handler which logs even debug messages
+fh = logging.FileHandler('../senko.log')
+fh.setLevel(logging.DEBUG)
+# create console handler with a higher log level
+ch = logging.StreamHandler()
+ch.setLevel(logging.WARNING)
+# create formatter and add it to the handlers
+formatter = logging.Formatter('[%(asctime)s] %(message)s')
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+# add the handlers to the logger
+log.addHandler(fh)
+log.addHandler(ch)
+
 
 # Import config file
 with open('config.yaml') as stream:
@@ -24,11 +43,11 @@ for file in filter(lambda file: file.endswith('.py'), os.listdir('./cogs')):
 # Log bot startup
 @bot.event
 async def on_ready() -> None:
-    print(f'We have logged in as {bot.user} in these servers:')
+    log.warning(f'We have logged in as {bot.user} in these servers:')
     for guild in bot.guilds:
-        print(f'{guild.name} ({guild.id})')
-    print(f'({len(bot.guilds)} servers)')
-    print()
+        log.warning(f'{guild.name} ({guild.id})')
+    log.warning(f'({len(bot.guilds)} servers)')
+    log.warning('************************')
 
 # Handle command cooldown
 @bot.event
